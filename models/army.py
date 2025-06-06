d3fa8f2d1c9350880dd6beece6c27000421c42f1
Morten Bendeke <mbendeke@hotmail.com>
2025-06-06 13:46:50 +0200

    more optional

--- models/army.py
+++ models/army.py
@@ -5,12 +5,12 @@ from typing import List, Optional
 
 
 class Army(BaseModel):
-    name: str
-    faction: str
+    name: Optional[str] = None
+    faction: Optional[str] = None
     formation: Optional[str] = None
-    points_limit: int
-    points_used: int
-    drops: int
+    points_limit: Optional[int] = 0
+    points_used: Optional[int] = 0
+    drops: Optional[int] = 0
     spell_lore: Optional[str] = None
     regiments: List[Regiment] = Field(default_factory=list)
     faction_terrain: Optional[str] = None
--- models/unit.py
+++ models/unit.py
@@ -1,12 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import List, Optional
 
 class Unit(BaseModel):
-    name: str
-    size: int
-    path: str
-    rank: int
-    abilities: list[str]
-    enchantments: list[str]
-    battle_wounds: int
-    battle_scars: list[str]
-    points: int
+    name: Optional[str] = None
+    size: Optional[int] = None
+    path: Optional[str] = None
+    rank: Optional[int] = None
+    abilities: List[str] = Field(default_factory=list)
+    enchantments: List[str] = Field(default_factory=list)
+    battle_wounds: Optional[int] = 0
+    battle_scars: List[str] = Field(default_factory=list)
+    points: Optional[int] = 0
