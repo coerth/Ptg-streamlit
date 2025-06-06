1d3b965814d2d1fb07d0c5e8b93991a754669b62
Morten Bendeke <mbendeke@hotmail.com>
2025-06-06 13:44:52 +0200

    optional fields

--- models/player.py
+++ models/player.py
@@ -1,13 +1,14 @@
 from pydantic import BaseModel
 from bson import ObjectId
 from models.army import Army
+from typing import Optional
+
 
 
 class Player(BaseModel):
-    name: str
-    army: Army
-    wins: int
-    losses: int
-    draws: int
-    played: int
-    
+    name: Optional[str] = None
+    army: Optional[Army] = None
+    wins: int = 0
+    losses: int = 0
+    draws: int = 0
+    played: int = 0
