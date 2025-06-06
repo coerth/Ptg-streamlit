e6080797c1418c2b3c297e793adada91e9304135
Morten Bendeke <mbendeke@hotmail.com>
2025-06-06 09:52:01 +0200

    proper streamlit syntax

--- pages/The Armies.py
+++ pages/The Armies.py
@@ -2,11 +2,11 @@ import streamlit as st
 from streamlit import session_state
 from db.player_functions import get_all_players
 
-st.widget("title", "The Armies")
-st.widget("header", "List of Armies")
+st.title("Armies Overview")
+st.header("List of All Players and Their Armies")
 players = get_all_players()
 if players:
     for player in players:
-        st.widget("subheader", f"Player: {player['name']}")
-        st.widget("markdown", f"- Army Name: **{player['army_name']}**")
-        st.widget("markdown", f"- Faction: **{player['faction']}**")
+        st.subheader(f"Player: {player['name']}")
+        st.markdown(f"- Army Name: **{player['army']['name']}**")
+        st.markdown(f"- Faction: **{player['army']['faction']}**")
