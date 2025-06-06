999cd3e653338bcdf737711a7be5fbae1ced5acd
Morten Bendeke <mbendeke@hotmail.com>
2025-06-06 09:57:13 +0200

    pydantic variables

--- pages/The Armies.py
+++ pages/The Armies.py
@@ -7,6 +7,6 @@ st.header("List of All Players and Their Armies")
 players = get_all_players()
 if players:
     for player in players:
-        st.subheader(f"Player: {player['name']}")
-        st.markdown(f"- Army Name: **{player['army']['name']}**")
-        st.markdown(f"- Faction: **{player['army']['faction']}**")
+        st.subheader(f"Player: {player.name}")
+        st.markdown(f"- Army Name: **{player.army.name}**")
+        st.markdown(f"- Faction: **{player.army.faction}**")
