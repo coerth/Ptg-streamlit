a62091b64a6a22161ee11f2bbd3a213991667475
Morten Bendeke <mbendeke@hotmail.com>
2025-06-06 09:52:56 +0200

    mongod collection

--- db/player_functions.py
+++ db/player_functions.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel, Field
 from typing import Optional
 from models.player import Player
 from models.army import Army
+from db.get_db import get_collection
 
 def create_player(players_collection: Collection, name: str, army_name: str, faction: str):
     new_player = Player(
@@ -18,7 +19,11 @@ def get_player_by_id(players_collection: Collection, player_id: str) -> Optional
         return Player(**player_data)
     return None
 
-def get_all_players(players_collection: Collection) -> list[Player]:
+def get_all_players() -> list[Player]:
+    players_collection = get_collection("players")
+    if not players_collection:
+        return []
+    
     players = []
     for player_data in players_collection.find():
         players.append(Player(**player_data))
